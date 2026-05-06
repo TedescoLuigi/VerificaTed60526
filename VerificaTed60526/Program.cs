29be451e9e85cf35a4c85277a2cd8cf8e3458db3
luigitedesco <luigitedesco@LUIGITEDESCB974>
2026-05-06 14:36:56 +0200

    .

--- VerificaTed60526/Program.cs
+++ VerificaTed60526/Program.cs
@@ -55,10 +55,48 @@ namespace VerificaTed60526
             }
 
             //Esercizio B: Verifica delle giocate vincenti
-            using (StreamWriter rr = new StreamWriter("estrazioni.csv"))
+            using (StreamReader giocate = new StreamReader("giocate_lotto_50.csv"))
+            using (StreamWriter finale = new StreamWriter("vincite.csv"))
             {
+                giocate.ReadLine();
 
+                string rigaGiocata;
+
+                while ((rigaGiocata = giocate.ReadLine()) != null)
+                {
+                    string[] parti = rigaGiocata.Split(',');
+
+                    string giocatore = parti[0];
+                    string ruota = parti[1];
+                    int numero = Convert.ToInt32(parti[2]);
+
+                    using (StreamReader estrazioni = new StreamReader("estrazioni.csv"))
+                    {
+                        estrazioni.ReadLine();
+
+                        string rigaEstrazione;
+
+                        while ((rigaEstrazione = estrazioni.ReadLine()) != null)
+                        {
+                            string[] dati = rigaEstrazione.Split(',');
+
+                            if (dati[0] == ruota)
+                            {
+                                for (int i = 1; i <= 5; i++)
+                                {
+                                    if (Convert.ToInt32(dati[i]) == numero)
+                                    {
+                                        finale.WriteLine(giocatore + " ha vinto con il numero " + numero);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
 }
+
+
+   
