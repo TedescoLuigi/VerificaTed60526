7ff9bc24f05ee764bd847be58398427b79cb8d7a
tedesco.luigi <tedesco.luigi@INF-1017>
2026-05-06 12:08:18 +0200

    Aggiungere i file di progetto.

---
+++ VerificaTed60526/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VerificaTed60526
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
